90b218050ab914a6375e87e0fbf7c26b51ae46a1
rpvarma01-jpg <158231282+rpvarma01-jpg@users.noreply.github.com>
2026-03-24 17:21:31 +0000

    Able to load article text. Next step is to be able to orgainize returned text.

---
+++ requests-tests.py
@@ -0,0 +1,79 @@
+import requests
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+
+
+
+class wikitool:
+    def __init__(self):
+        
+         
+        self.header = {
+        "User-Agent": 'SchoolProject-COMPSCI/1.0 (High School Project; Fairveiw High School; mailto: rpvarma01@bvsd.org)'
+        }
+        self.articles = [];
+        self.choice_index = 0;
+        self.title = "";
+        self.article_chosen = False;
+        self.fullText = "";
+         
+    def wiki_search(self, search):
+        params_search = {
+            "action": "query",
+            "list": "search",
+            "srsearch": search,
+            "srlimit": 10,
+            "format": "json"}
+        
+        response = requests.get(URL, headers= self.header, params = params_search)
+        response.raise_for_status()
+
+        data  = response.json()
+
+        titles = [item["title"] for item in data["query"]["search"]]
+        self.articles = titles;
+        return self.articles;
+
+
+    def wiki_choose(self, choice):
+        if (self.articles == []):
+            return "error no loaded article"
+        else:
+            self.choice_index = choice;
+            self.article_chosen = True;
+            self.title = self.articles[self.choice_index];
+
+    def wiki_load_text_without_header(self):
+        if (self.article_chosen != True):
+            return "error no chosen article to be loaded from article list"
+        
+        
+        title = self.title;
+        params_load = {
+            "action": "query",
+            "titles": title,
+            "prop": "extracts",
+            "explaintext":True,
+            "exsectionformat": "wiki",
+            "exlimit":"max",
+            "format": "json"
+
+        }
+        response  = requests.get(URL, headers = self.header, params = params_load)
+        response.raise_for_status()
+        data = response.json()
+        pages = data["query"]["pages"]
+        page_id = list(pages.keys())[0] 
+        full_text = pages[page_id]["extract"]
+        self.fullText = full_text;
+        return self.fullText;
+        
+    
+bot = wikitool()
+bot.wiki_search("Mars")
+bot.wiki_choose(0)
+print(bot.wiki_load_text_without_header())
+
+
+
